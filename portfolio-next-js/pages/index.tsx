67705410d8c62063f5a025dd6862c7b7b8357895
Bandla <rbandla@albany.edu>
2022-12-12 15:05:01 -0500

    fixed popup error

--- portfolio-next-js/pages/index.tsx
+++ portfolio-next-js/pages/index.tsx
@@ -1,13 +1,21 @@
+import dynamic from 'next/dynamic'
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
-import About from '../components/About'
-import ContactMe from '../components/ContactMe'
-import Header from '../components/Header'
-import Hero from '../components/Hero'
-import Projects from '../components/Projects'
-import Skills from '../components/Skills'
-import WorkExperience from '../components/WorkExperience'
+// import About from '../components/About'
+const  About = dynamic(() => import('../components/About'), { ssr: false })
+// import ContactMe from '../components/ContactMe'
+const  ContactMe = dynamic(() => import('../components/ContactMe'), { ssr: false })
+// import Header from '../components/Header'
+const  Header = dynamic(() => import('../components/Header'), { ssr: false })
+// import Hero from '../components/Hero'
+const  Hero = dynamic(() => import('../components/Hero'), { ssr: false })
+// import Projects from '../components/Projects'
+const  Projects = dynamic(() => import('../components/Projects'), { ssr: false })
+// import Skills from '../components/Skills'
+const  Skills = dynamic(() => import('../components/Skills'), { ssr: false })
+// import WorkExperience from '../components/WorkExperience'
+const  WorkExperience = dynamic(() => import('../components/WorkExperience'), { ssr: false })
 
 
 export default function Home() {
